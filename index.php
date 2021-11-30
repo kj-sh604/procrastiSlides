8db0ed5e281493214f5f282403a64125baffcfbc
Blista Kanjo <43.splash@gmail.com>
2021-11-30 06:12:52 -0500

    [Revision 5] clearer instructions/how-to for users

--- index.php
+++ index.php
@@ -96,39 +96,35 @@ session_start();
 
          <br>
 
-                  <a name="how-to"><h2 class="centered">how to:</h2></a>
+                  <a name="how-to"><h2 class="centered">quick read how-to:</h2></a>
                   <ul>
                     <li>type '#' to indicate that you are starting a new slide and then press ↵ (enter/return) twice.</li>
-                    <li>you can add a "section title" after each '#' to name that portion of your presentation.</li>
+                    <li>you can add a "slide title" after each '#' to name that portion of your presentation.</li>
                     <ul>
-                      <li>if you choose to add a section title, a space between "#" and your slide title is required:</li>
+                      <li>a space between "#" and your slide title is required: <br><br> </li>
                       <ul class="no-bullets">
-                        <li> <pre>
+                        <li># [YOUR TITLE GOES HERE]</li>
 
-                      # [YOUR TITLE GOES HERE]
-                        </pre> </li>
-
-                      </ul>
+                      </ul><br>
                     </ul>
                     <li>type/format text underneath the '#' for the slide contents.</li>
                     <ul>
                       <li>make sure to start typing your slide content two ↵'s ("enter key presses") away from the "#".</li>
-                      <li>lists require to be formatted in this manner as well, like so:</li>
+                      <li>lists require to be formatted in this manner as well: <br><br> </li>
                       <ul class="no-bullets">
-                        <li> <pre>
+                        <li># slide title <br><br>
 
-                            # slide title
+                            This is a list <br><br>
 
-                            This is a list
-
-                            * item 1
-                            * item 2
-                            * item 3
-                        </pre> </li>
+                            * item 1 <br>
+                            * item 2 <br>
+                            * item 3 <br>
+                     </li>
 
                       </ul>
                     </ul>
                   </ul>
+                  <br>
 
                   <p class="centered"> <i>It's highly recommended that you use markdown lists rather than plain text lists</i> </p>
 
