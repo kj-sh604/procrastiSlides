86533f412a4bea643d99a794fcf655827163decf
Blista Kanjo <43.splash@gmail.com>
2023-05-08 17:35:29 -0400

    refactor: updated github information

--- includes/nav-footer.php
+++ includes/nav-footer.php
@@ -4,5 +4,5 @@
 <a href="../info.php">info</a>
 <a href="../licenses.php">licenses</a>
 <br>
-<p>All site content is in the Public Domain.</p>
-<p><small>Powered by <a href="https://github.com/blistakanjo604">github/blistakanjo604</a></small></p>
+<p>all site content is in the public domain</p>
+<p><small>github: <a href="https://github.com/kj-sh604" target="_blank">kj-sh604</a></small></p>
