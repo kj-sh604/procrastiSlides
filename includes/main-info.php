a29677f19edf5a3c3059f76f2ccc4fb62356ad09
kj-sh604 <43.splash@gmail.com>
2024-07-03 20:49:46 -0400

    kj-gitbot: includes/main-info.php

---
+++ includes/main-info.php
@@ -0,0 +1,4 @@
+<p class="centered">simple presentation(s) site for quick slides from markdown <br>
+    (powered by LaTeX beamer, pandoc, and the rust uutils)</p>
+<p class="centered"><b>✨ latest feature update:</b> addition of 16:9 themes 🚀</p>
+<hr>
