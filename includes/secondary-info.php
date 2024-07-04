13e03b275ff1893b877e61d0b05357131260dca0
kj-sh604 <43.splash@gmail.com>
2024-07-03 20:49:48 -0400

    kj-gitbot: includes/secondary-info.php

---
+++ includes/secondary-info.php
@@ -0,0 +1,4 @@
+<p class="centered">this website is inspired from the suckless's presentation tool (<a target="_blank" href="https://tools.suckless.org/sent">sent</a>).
+    <br /> it uses pandoc and LaTeX beamer for the backend. <br><br><b>latest feature update:</b> addition of 16:9 themes
+</p>
+<hr>
