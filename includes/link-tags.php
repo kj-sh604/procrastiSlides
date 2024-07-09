d259fcde814ab8dc4b393c944649055b840d011d
kj-sh604 <43.splash@gmail.com>
2024-07-09 02:09:47 -0400

    kj-gitbot: includes/link-tags.php

---
+++ includes/link-tags.php
@@ -0,0 +1,3 @@
+<link rel="icon" href="../img/site-icon.webp" type="image/x-icon">
+<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="https://cdn.rawgit.com/xcatliu/simplemde-theme-dark/master/dist/simplemde-theme-dark.min.css">
