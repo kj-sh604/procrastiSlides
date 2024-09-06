ae127841eb9e45878331200fb3868bc7a65ef68d
kj-sh604 <43.splash@gmail.com>
2024-09-05 20:15:35 -0400

    fix: footer centering

--- includes/nav-footer.php
+++ includes/nav-footer.php
@@ -1,7 +1,7 @@
-<hr/>
+<hr />
 <a href="../index.php">home</a> &nbsp;
 <a href="../info.php">info</a> &nbsp;
-<a href="../licenses.php">licenses</a> &nbsp;
+<a href="../licenses.php">licenses</a>
 <br>
 <p>all site content is in the public domain</p>
 <p><small>my other stuff: <a href="https://aedrielkylejavier.me/" target="_blank">website link →</a></small></p>
