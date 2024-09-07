eaaf803a316f4ed4bc2677d1874b7cf15754b921
kj-sh604 <43.splash@gmail.com>
2024-09-07 02:00:29 -0400

    revert: `ae127841eb9e45878331200fb3868bc7a65ef68d`

--- includes/nav-footer.php
+++ includes/nav-footer.php
@@ -1,7 +1,7 @@
 <hr />
-<a href="../" class="footer-no-underline">🏠 home</a> &nbsp;
-<a href="../info.php" class="footer-no-underline">🗂️ info</a> &nbsp;
-<a href="../licenses.php" class="footer-no-underline">⚖️ licenses</a>
+<a href="../">home</a> &nbsp;
+<a href="../info.php">info</a> &nbsp;
+<a href="../licenses.php">licenses</a>
 <br>
 <p>all site content is in the public domain</p>
 <p><small>my other stuff: <a href="https://aedrielkylejavier.me/" target="_blank">website link →</a></small></p>
