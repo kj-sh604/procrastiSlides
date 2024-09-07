78e60b2d13b68c79f59ea79696c7dbc6a5cd2eb4
kj-sh604 <43.splash@gmail.com>
2024-09-07 01:51:10 -0400

    refactor: match footer styling with header
    also updated anchor links

--- includes/nav-header.php
+++ includes/nav-header.php
@@ -1,6 +1,6 @@
 <div class="centered">
    <h2>
-      <a style="text-decoration: none;" href="../index.php">🏠 home</a>
+      <a style="text-decoration: none;" href="../">🏠 home</a>
       <a style="text-decoration: none;" href="../info.php">🗂️ info</a>
       <a style="text-decoration: none;" href="../licenses.php">⚖️ licenses</a>
    </h2>
