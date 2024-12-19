2416b24315c115e8b03d7bb75bcf05ce949e04d1
kj-sh604 <43.splash@gmail.com>
2024-12-19 04:17:12 -0500

    kj-gitbot: vids/

---
+++ vids/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="color-scheme" content="light dark">
+    <meta http-equiv="refresh" content="0; URL='https://youtu.be/XGxIE1hr0w4'" />
+    <title>Art and Assets</title>
+</head>
+<body>
+    <p>If you are not redirected, <a href="https://youtu.be/XGxIE1hr0w4">click here for folder contents</a>.</p>
+</body>
+</html>
