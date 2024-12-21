8b8626c0c7cfef1777c524f6eada9f326ff59cd3
kj-sh604 <43.splash@gmail.com>
2024-12-21 03:56:20 -0500

    refactor: make `.ogv` video as final fallback
    also update file extension and encoding

--- includes/video-how-to.php
+++ includes/video-how-to.php
@@ -3,8 +3,8 @@
                 <p>
                     <video controls>
                         <source src="vids/A4upxlH2mIHOXYRq.webm" type="video/webm">
-                        <source src="vids/A4upxlH2mIHOXYRq.ogg" type="video/ogg">
                         <source src="vids/A4upxlH2mIHOXYRq.mp4" type="video/mp4">
+                        <source src="vids/A4upxlH2mIHOXYRq.ogv" type="video/ogg">
                         Your browser does not support modern open-source video implementations 😔.
                     </video>
                 </p>
