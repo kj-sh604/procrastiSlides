91eebe5fa32d7837cbda24a5a74c302985d8461e
kj-sh604 <43.splash@gmail.com>
2025-03-21 03:09:03 -0400

    feat: updated video

--- includes/video-how-to.php
+++ includes/video-how-to.php
@@ -2,9 +2,9 @@
                 <summary style="font-size: 1.125rem;"><strong>← expand for a quick video demo 🎬</strong></summary>
                 <p>
                     <video controls>
-                        <source src="vids/A4upxlH2mIHOXYRq.webm" type="video/webm">
-                        <source src="vids/A4upxlH2mIHOXYRq.mp4" type="video/mp4">
-                        <source src="vids/A4upxlH2mIHOXYRq.ogv" type="video/ogg">
+                        <source src="vids/6iaXPNqAwksiXQnb.webm" type="video/webm">
+                        <source src="vids/6iaXPNqAwksiXQnb.mp4" type="video/mp4">
+                        <source src="vids/6iaXPNqAwksiXQnb.ogv" type="video/ogg">
                         Your browser does not support modern open-source video implementations 😔.
                     </video>
                 </p>
