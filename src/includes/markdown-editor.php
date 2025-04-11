7d4b6cea82bff234c7fd2362fddc1c8ccb4882ee
kj-sh604 <43.splash@gmail.com>
2025-04-11 16:43:54 -0400

    refactor: rem adjustment

--- src/includes/markdown-editor.php
+++ src/includes/markdown-editor.php
@@ -3,7 +3,7 @@
                 <h2 class="centered">markdown goes here:</h2>
             </label>
             <form action="index.php" method="post">
-                <textarea id="user-input" name="user-input" style="width: 50rem; height: 25.625rem;"><?php echo $_SESSION['user-input'] ?? ''; ?></textarea>
+                <textarea id="user-input" name="user-input" style="width: 50rem; height: 25.5rem;"><?php echo $_SESSION['user-input'] ?? ''; ?></textarea>
                 <div class="centered">
                     <button type="submit" name="compile">create presentation</button>
                 </div>
