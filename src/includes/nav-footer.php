beaabd5b2f719a62526d5b4214cfc2bc4fab41aa
kj_sh604 <43.splash@gmail.com>
2025-12-31 08:11:26 -0500

    refactor: use datever²

--- src/includes/nav-footer.php
+++ src/includes/nav-footer.php
@@ -5,4 +5,11 @@
 <br>
 <p>all site content is in the public domain</p>
 <p><small>my other stuff: <a href="https://aedrielkylejavier.me/" target="_blank">website link →</a></small></p>
-<p><small>version: <?php echo file_get_contents(__DIR__ . '/../VERSION'); ?></small></p>
+<p><small><pre>version: <?php
+$version_file = __DIR__ . '/../VERSION';
+if (file_exists($version_file)) {
+    echo htmlspecialchars(trim(file_get_contents($version_file)), ENT_QUOTES, 'UTF-8');
+} else {
+    echo 'unknown';
+}
+?></pre></small></p>
