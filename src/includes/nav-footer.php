7e02f5bb190837e3d05713e6fc98db8aeb173563
kj_sh604 <43.splash@gmail.com>
2026-01-01 01:21:47 -0500

    refactor: use `<code>` for version block

--- src/includes/nav-footer.php
+++ src/includes/nav-footer.php
@@ -5,11 +5,11 @@
 <br>
 <p>all site content is in the public domain</p>
 <p><small>my other stuff: <a href="https://aedrielkylejavier.me/" target="_blank">website link →</a></small></p>
-<p><small><pre>version: <?php
+<p><small><code>version: <?php
 $version_file = __DIR__ . '/../VERSION';
 if (file_exists($version_file)) {
     echo htmlspecialchars(trim(file_get_contents($version_file)), ENT_QUOTES, 'UTF-8');
 } else {
     echo 'unknown';
 }
-?></pre></small></p>
+?></code></small></p>
