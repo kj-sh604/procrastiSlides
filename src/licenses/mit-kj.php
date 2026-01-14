8bc0dd46ef02e0bc179a492bbf5b33f223c3d1bb
kj_sh604 <43.splash@gmail.com>
2026-01-13 22:26:04 -0500

    kj-gitbot: src/licenses/mit-kj.php

---
+++ src/licenses/mit-kj.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include "../includes/site-title-and-description.php"; ?>
+    <?php include "../includes/link-tags-in-head.php"; ?>
+    <meta name="color-scheme" content="light dark">
+</head>
+
+<body>
+    <?php
+    include "../includes/license-banner.php";
+    include "../includes/secondary-info.php";
+    include "../includes/nav-header.php";
+    ?>
+    <div class="centered">
+        <pre>
+
+MIT License
+
+Copyright (c) 2026 kj_sh604/kj-sh604/kjsh604
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
+      </pre>
+    </div>
+
+    <br>
+    <footer>
+
+        <?php include "../includes/nav-footer.php"; ?>
+
+
+    </footer>
+</body>
+
+</html>
