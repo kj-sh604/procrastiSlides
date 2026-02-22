4e842852b13d84daede1b6b5db5c592fab4d3ff5
kj_sh604 <43.splash@gmail.com>
2026-02-22 02:52:34 -0500

    refactor: images fix

--- src/output/index.php
+++ src/output/index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="color-scheme" content="light dark">
-    <meta http-equiv="refresh" content="0; URL='https://youtu.be/XGxIE1hr0w4'" />
-    <title>Art and Assets</title>
+    <link rel="icon" href="/img/site-icon.webp" type="image/x-icon">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>error producing pdf</title>
 </head>
 <body>
-    <p>If you are not redirected, <a href="https://youtu.be/XGxIE1hr0w4">click here for folder contents</a>.</p>
+    <h1>error producing pdf</h1>
+    <div class="error-box">
+        <p><strong>sorry, there was an error generating your pdf.</strong></p>
+        <p>please check your input and try again.</p>
+    </div>
 </body>
 </html>
